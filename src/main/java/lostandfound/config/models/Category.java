075663bc645447c442c8bb3a6b35e82bc8a72994
sahil pal <sahil.sp.sp.2003@gmail.com>
2025-07-08 16:23:35 +0530

    created Category object

--- src/main/java/lostandfound/config/models/Category.java
+++ src/main/java/lostandfound/config/models/Category.java
@@ -1,4 +1,11 @@
 package lostandfound.config.models;
 
+import io.vertx.core.json.JsonObject;
+
 public class Category {
+    public static JsonObject toMongoDoc(JsonObject body) {
+        return new JsonObject()
+                .put("name", body.getString("name"))
+                .put("description", body.getString("description", ""));
+    }
 }
